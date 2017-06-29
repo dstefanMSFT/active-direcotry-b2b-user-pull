3d1de056894c6610cae0e4616fa98459bdb7322c
Daniel Stefaniak <dstefan@microsoft.com>
2017-06-29 15:48:17 -0700

    Push

---
+++ AppProxy-GuestShadowAccountCreation-v1.0.2.ps1
@@ -0,0 +1,205 @@
+﻿<# 
+ 
+.SYNOPSIS
+    Sample script to create a differencing list between Guest accounts in an Azure AD tenant and an on-prem Active Directory OU
+    Includes options to 
+    - create users in the OU who exist in AAD and are members of a specified group but are not present in the AD
+    - optionally disable and move shadow accounts in the OU who no longer exist in Azure AD to a different OU
+    - optionally delete shadow accounts in the OU who no longer exist in Azure AD
+
+    This would be used to create shadow accounts in AD for use by Application Proxy for KCD delegation for B2B Guest accounts.
+
+    The shadow account will be created with the following properties:
+            -AccountPassword = random strong password
+            -ChangePasswordAtLogon = $false
+            –PasswordNeverExpires = $true
+            -SmartcardLogonRequired = $true
+
+    NOTE - this does not have support for nesting in the AAD Group
+
+	Ian Parramore ianparr@microsoft.com
+
+.DESCRIPTION
+
+    Version: 1.0.1
+
+    This is currently a beta level script and intended to be used as a demonstration script
+
+.DISCLAIMER
+    THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF
+    ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+    THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+    PARTICULAR PURPOSE.
+
+    Copyright (c) Microsoft Corporation. All rights reserved.
+#>
+
+
+<#
+BUGBUG
+
+- add cert based auth
+- decide on where and whether to filter on active/disabled AAD status
+- should we use V1 or V2 AAD PSH cmdlets
+- how should we populate the samAccountName? At the moment I take first 20 characters of the UPN
+- if the B2B user is re-invited after having been deleted and the shadow account is archived, recreation of a new shadow accounn will fail. 
+  Should we handle this? We could check the archive OU and move the account back. THoughts?
+- need error handling if Add-AdUser call fails
+- add reporting - should be off by default but available for troubleshooting
+- clean up telemetry
+
+/BUGBUG
+#>
+
+
+
+# Set up variables
+
+# This needs updating to target the relevent group
+# $B2BGroupSid = "97623264-4f72-46cf-b4e5-1205bb57f4a3"
+$B2BGroupSid = "7940bfb4-9b68-4c19-8180-aeb6fab569fd"
+
+$ShadowAccountOU = "OU=ShadowAccounts,DC=corp,DC=pfe,DC=ninja"
+$ShadowAccountOUArchive = "OU=Archive,OU=ShadowAccounts,DC=corp,DC=pfe,DC=ninja"
+$createmissingshadowaccounts = $true
+
+# only one of the following should be true. If both are True then disable action takes precedence
+$disableorphanedshadowaccounts = $false
+$deleteorphanedshadowaccounts = $false
+
+
+$TenantGuestUsersHash = @{} 
+$UsersInB2BGroupHash = @{}
+$B2bShadowAccountsHash = @{}
+
+# to do - add Cert based authentication
+# at the moment we will manually auth using ADAL duing the Connect-MsolService call
+# Connect-MsolService
+
+$appID = "a37744ce-8849-4567-bee5-93ca719848ec" # Insert your application's Client ID, a Globally Unique ID (registered by Global Admin)
+$appSecret = "bnxyQLlqJ7E0lI38Z+TtNk65VA4aqbmF/2se8RJuJsk="  # Insert your application's Client Key/Secret string
+$loginURL       = "https://login.microsoftonline.com/" # AAD Instance; for example https://login.microsoftonline.com
+$tenantdomain   = "pfeninja.onmicrosoft.com"    # AAD Tenant; for example, contoso.onmicrosoft.com
+$resource       = "https://graph.windows.net"
+$body=$creds
+$body = @{grant_type="client_credentials";resource=$resource;client_id=$appID;client_secret=$appSecret}
+$oauth      = Invoke-RestMethod -Method Post -Uri $loginURL/$tenantdomain/oauth2/token?api-version=1.5 -Body $body 
+Connect-AzureAD -AadAccessToken $oauth.access_token -TenantId 944d9418-0d20-4e86-9e1d-dce70945d8f1 -AccountId $appID
+
+
+# Populate hash table with all Guest users from tenant using object ID as key
+# This is to make just one MSOL-User call fore efficiency purposes
+Get-AzureADUser -All $true -Filter "userType eq 'Guest'" |  `
+    ForEach-Object {$TenantGuestUsersHash[$_.ObjectId] = $_}
+
+# Populate hash table with membership of target group from Azure AD using object ID as key
+# we will then reference across into the Guest use hash table as needed.
+ Get-AzureADGroupMember -ObjectId $B2BGroupSid -all $true | `
+        ForEach-Object {$UsersInB2BGroupHash[$_.ObjectId] = $_}
+
+# Populate hash table with all accounts in shadow account OU using UPN as key
+# consider setting value to Null instead of the object as we don't use this
+Get-AdUser -filter * -SearchBase $ShadowAccountOU | `
+Select-Object UserPrincipalName, Name, Description | ` 
+ForEach-Object {$B2bShadowAccountsHash[$_.UserPrincipalName] = $_}
+
+
+# For each tenant Guest account UPN in the group check if it exists in SHadow OU hash table
+# If exists then remove from both lists
+# End state of B2B Group list will be all tenant guest accounts in the group not in the shadow OU
+# End state of shadow account list will be all shadow accounts without a matching tenant guest account
+
+
+ForEach($key in $($UsersInB2BGroupHash.Keys))
+    {
+
+    
+    # remove non-guest users from the AAD Group list
+    if($TenantGuestUsersHash.ContainsKey($key) -eq $false)
+        {
+        # if we want to output anything about non-Guest users it needs to pull from the Group Membership hash 
+        # as these users will not be in the guest users hash table e.g. 
+        # $UsersInB2BGroupHash[$key].emailaddress
+
+        $UsersInB2BGroupHash.Remove($key)
+        }
+
+    # B2B guest user already has a shadow account remove from both lists
+    # we'll then end up with 2 differencing lists
+    elseif ($B2bShadowAccountsHash.ContainsKey($TenantGuestUsersHash[$key].userprincipalname))
+        {
+        # $TenantGuestUsersHash[$key].userprincipalname
+        # Write-Host $key "account exists in both AAD group and AD OU - removing from both lists"
+        $UsersInB2BGroupHash.Remove($key)
+        $B2bShadowAccountsHash.Remove($key)
+        }
+    }
+    
+<#
+Write-Host ""
+Write-Host "*****Tenant Object ID's that need shadow accounts creating*****"
+$UsersInB2BGroupHash.Keys
+Write-Host ""
+Write-Host "*****Shadow account UPNs with no matching tenant account - review for removal*****"
+$B2bShadowAccountsHash.Keys
+Write-Host ""
+#>
+
+If ($createmissingshadowaccounts -eq $true)
+{
+    ForEach($key in $($UsersInB2BGroupHash.keys))
+        {
+        $key
+        $TenantGuestUsersHash[$key].UserPrincipalName
+
+        $samaccountname = $TenantGuestUsersHash[$key].userprincipalname.Substring(0, 20)
+        $samaccountname 
+
+        # generate random password
+        $bytes = New-Object Byte[] 32
+        $rand = [System.Security.Cryptography.RandomNumberGenerator]::Create()
+        $rand.GetBytes($bytes)
+        $rand.Dispose()
+        $RandPassword = [System.Convert]::ToBase64String($bytes)
+            
+        New-ADUser -Name $samaccountname `
+            -SamAccountName $samaccountname `
+            -Path $ShadowAccountOU `
+            -UserPrincipalName $TenantGuestUsersHash[$key].userprincipalname `
+            -Description $TenantGuestUsersHash[$key].userprincipalname `
+            -DisplayName $TenantGuestUsersHash[$key].Value.DisplayName `
+            -AccountPassword (ConvertTo-SecureString $RandPassword -AsPlainText -Force) `
+            -ChangePasswordAtLogon $false `
+            –PasswordNeverExpires $true `
+            -SmartcardLogonRequired $true `
+        }
+        Enable-ADAccount -Identity $samaccountname
+}
+
+
+# clean up time for any Shadow Accounts where the AAD guest account no longer exists
+ If ($disableorphanedshadowaccounts -eq $true -or $deleteorphanedshadowaccounts -eq $true)
+ {
+
+      ForEach ($shadow in $($B2bShadowAccountsHash.keys))
+        {
+            # $upn = the key from B2bShadowAccountsHash = $shadow
+            # disable operation takes precedence over deletion
+            If ($disableorphanedshadowaccounts -eq $true)
+            {
+                Get-AdUser -Filter {UserPrincipalName -eq $shadow} -SearchBase $ShadowAccountOU| Set-ADUser -Enabled $false -Description 'Disabled pending removal' 
+                Get-AdUser -Filter {UserPrincipalName -eq $shadow} -SearchBase $ShadowAccountOU | Move-ADObject -TargetPath $ShadowAccountOUArchive
+                
+            }
+            ElseIf ($deleteorphanedshadowaccounts = $true)
+            {
+                Get-AdUser -Filter {UserPrincipalName -eq $shadow} -SearchBase $ShadowAccountOU | Remove-AdUser
+            }
+        }
+
+  }
+
+
+
+
+
--- README.md
+++ README.md
@@ -1,2 +1,94 @@
-# active-direcotry-b2b-user-pull
+# Script for pulling AAD B2B users into on-premises Active Directory
 Script sample for pulling AAD b2b users into on-premises AD
+## Pre-requisites
+### Create new AAD Application registration
+* Log into the [Azure AD Management Portal: App Registrations](https://aad.portal.azure.com/#blade/Microsoft_AAD_IAM/ApplicationsListBlade) and Click "+ New application registration" and enter the name of your app
+
+  ![App Registrations](DocImages/snip1.png)
+
+* Select "Web app / API", and enter the Sign-on URL. If you're setting this up before you deploy the app to Azure, you can enter https://loopback as a placeholder. Click "Create".
+
+  ![App properties](DocImages/snip2.png)
+
+* From the application list, find the app you just created and click to open and edit it
+* Click on "Required permissions", then click "+ Add". On "Select an API", click and select "Microsoft Graph"
+* Click "Select permissions". On the "Enable Access" panel that appears, check the following items:
+  * APPLICATION PERMISSIONS
+    * Read Directory Data
+* Click "Select"
+
+  ![App permissions](DocImages/snip3.png)
+
+* Back on the "Required permissions" panel, click the "Grant Permissions" button at the top.
+
+  ![App permissions](DocImages/snip4.png)
+
+* Back on the Settings panel, click "Keys". Under Description, enter a name for the application key, like "Key 1". Under Expires, select 1 or 2 years. (NOTE: you or someone in your organization will need to make a note to come back and refresh this key before it expires.)
+* Click "Save". An application secret will be generated and displayed. COPY this key and record it - you'll need it in an minute when setting up the web application. 
+>[!NOTE]
+>This key will not be displayed again and cannot be retrieved. If you lose it, you'll have to come back, delete it, and create another one.
+
+   ![App permissions](DocImages/snip5.png)
+
+* Finally, before we are done with the first app, record the "Application ID". You can click to the right of it in the main panel and it will copy it to your clipboard. Record it along with the app secret from above - these two strings will be needed to setup the web app.
+
+   ![App permissions](DocImages/snip6.png)
+
+### Install required Powershell modules
+
+Script uses two Powershell Modules:
+* Active Directory 
+```powershell
+Install-WindowsFeature -Name RSAT-AD-PowerShell
+```
+* Azure Active Directory v2 Preview:
+```powershell
+install-module azureadpreview `
+-Force `
+-MinimumVersion 2.0.0.127 `
+-SkipPublisherCheck
+```
+>[!NOTE]
+>Above command requires Windows Server 2016 or [Windows Management Framework 5.0](https://www.microsoft.com/en-us/download/details.aspx?id=50395)
+
+### Create Azure AD Group for guests that should be pulled into on-premises
+To prevent pulling all users into on-premises environment, sample script filters guest users by group membership. 
+
+### Optional - Service account for running script schaduled task
+We recommend using Group Managed Service Accounts.
+```
+#running this command requires Domain Administrator Credentials
+$cpu = Get-ADComputer ComputerName #Server that will be running the script
+$acctName = "gmsa_b2b_script"
+New-ADServiceAccount -Description "Account for creating B2B users" `
+-DisplayName $acctName `
+-DNSHostName "$acctName.contoso.com" `
+-Name $acctName `
+-PrincipalsAllowedToRetrieveManagedPassword $cpu
+
+install-adserviceaccount $acctName
+```
+
+### Optional - register scheduled task
+Task for running script on a schedule
+```powershell
+$action = New-ScheduledTaskAction -Execute powershell.exe `
+-Argument "-NonInteractive -NoLogo -NoProfile -File c:\scripts\AppProxy-GuestShadowAccountCreation-v1.0.2.ps1"
+$trigger = New-ScheduledTaskTrigger -At 7:00 -Daily
+$principal = New-ScheduledTaskPrincipal -UserId corp\gmsa_b2b_script$ -LogonType Password
+Register-ScheduledTask ShadowB2BUsers `
+-Principal $principal `
+-Action $action `
+-Trigger $trigger
+```
+
+>[!IMPORTANT]
+>Script requires editing. Run it manually before running the scheduled task
+
+### Optional Delegate permissions to target OU for GMSA
+Account running script requires write permissions for OU dedicated for shadow accounts
+```powershell
+dsacls.exe "OU=ShadowAccounts,DC=corp,DC=contoso,DC=com" /G 'corp\gmsa_b2b_script$:GA'
+```
+
+## Considerations
